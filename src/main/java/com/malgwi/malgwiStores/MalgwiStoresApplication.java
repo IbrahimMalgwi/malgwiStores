1d3c169507257377f82fe424d73fa7ac897445e3
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-01-30 01:10:55 +0100

    first commit

---
+++ src/main/java/com/malgwi/malgwiStores/MalgwiStoresApplication.java
@@ -0,0 +1,13 @@
+package com.malgwi.malgwiStores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalgwiStoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalgwiStoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malgwi/malgwiStores/MalgwiStoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malgwi.malgwiStores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalgwiStoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
